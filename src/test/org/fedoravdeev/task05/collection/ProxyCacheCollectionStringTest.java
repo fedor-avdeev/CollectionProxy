3b524d0fe6b74fd831bff0cc393592699a6274a6
Fedor Avdeev <fedor.avdeev@gmail.com>
2019-03-18 23:16:34 +0200

    added assert

--- src/test/org/fedoravdeev/task05/collection/ProxyCacheCollectionStringTest.java
+++ src/test/org/fedoravdeev/task05/collection/ProxyCacheCollectionStringTest.java
@@ -1,34 +1,38 @@
 package org.fedoravdeev.task05.collection;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ProxyCacheCollectionStringTest {
-	
-	@InjectMocks
+
 	CollectionString proxyCacheCollectionString = mock(ProxyCacheCollectionString.class);
-	
-	@Test
-	public void shouldGetCountUniqueCharactersFromProxy () {
-		proxyCacheCollectionString.getString("hello world!");
-		when(proxyCacheCollectionString.getString("hello world!"))
-				.thenReturn("\"h\" - 1\n" +
+    @Test
+    public void shouldGetCountUniqueCharactersFromProxy () {      	
+    	
+    	when(proxyCacheCollectionString.getString("hello world!")).thenReturn("\"h\" - 1\n" +
 				"\"e\" - 1\n" +
 				"\"l\" - 3\n" +
 				"\"o\" - 2\n" +
 				"\" \" - 1\n" +
-				"\"w\" - 1\n" +
-				"\"r\" - 1\n" +
-				"\"d\" - 1\n" +
-				"\"!\" - 1\n");
-		
-		verify(proxyCacheCollectionString).getString("hello world!");
-	}
-}
+				"\"w\" - 1\n" +	
+				"\"r\" - 1\n" +	
+				"\"d\" - 1\n" + 
+				"\"!\" - 1\n"); 
+	
+    	assertEquals(proxyCacheCollectionString.getString("hello world!"),"\"h\" - 1\n" +
+				"\"e\" - 1\n" +
+				"\"l\" - 3\n" +
+				"\"o\" - 2\n" +
+				"\" \" - 1\n" +
+				"\"w\" - 1\n" +	
+				"\"r\" - 1\n" +	
+				"\"d\" - 1\n" + 
+				"\"!\" - 1\n"); 
+
+    	
+    	verify(proxyCacheCollectionString).getString("hello world!");
+    }
