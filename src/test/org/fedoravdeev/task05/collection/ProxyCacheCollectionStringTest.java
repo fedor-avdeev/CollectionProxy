08ecc6125761698c08f4d247ec1ca54e76cef03a
Fedor Avdeev <fedor.avdeev@gmail.com>
2019-03-19 09:04:59 +0200

    added argument capture

--- src/test/org/fedoravdeev/task05/collection/ProxyCacheCollectionStringTest.java
+++ src/test/org/fedoravdeev/task05/collection/ProxyCacheCollectionStringTest.java
@@ -1,38 +1,28 @@
 package org.fedoravdeev.task05.collection;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProxyCacheCollectionStringTest {
-
-	CollectionString proxyCacheCollectionString = mock(ProxyCacheCollectionString.class);
-    @Test
-    public void shouldGetCountUniqueCharactersFromProxy () {      	
-    	
-    	when(proxyCacheCollectionString.getString("hello world!")).thenReturn("\"h\" - 1\n" +
-				"\"e\" - 1\n" +
-				"\"l\" - 3\n" +
-				"\"o\" - 2\n" +
-				"\" \" - 1\n" +
-				"\"w\" - 1\n" +	
-				"\"r\" - 1\n" +	
-				"\"d\" - 1\n" + 
-				"\"!\" - 1\n"); 
 	
-    	assertEquals(proxyCacheCollectionString.getString("hello world!"),"\"h\" - 1\n" +
-				"\"e\" - 1\n" +
-				"\"l\" - 3\n" +
-				"\"o\" - 2\n" +
-				"\" \" - 1\n" +
-				"\"w\" - 1\n" +	
-				"\"r\" - 1\n" +	
-				"\"d\" - 1\n" + 
-				"\"!\" - 1\n"); 
-
-    	
-    	verify(proxyCacheCollectionString).getString("hello world!");
-    }
+	@Mock
+	CollectionString storeCollectionString;
+	
+	@InjectMocks
+	CollectionString proxyCacheCollectionString = mock(ProxyCacheCollectionString.class);
+	
+	@Test
+	public void shouldGetCountUniqueCharactersFromProxy () {		
+		proxyCacheCollectionString.getString("hello world!");
+		ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+		verify(proxyCacheCollectionString).getString(argumentCaptor.capture());
+	}
+}
